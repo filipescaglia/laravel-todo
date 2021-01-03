711b63da9e132856ccac5b16661c5c94cf503c4b
Filipe Scaglia <filipecscaglia@gmail.com>
2021-01-03 16:27:23 -0300

    Feature: store task

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Todo;
 
 class ApiController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $response = ['error' => ''];
+
+        $rules = [
+            'title' => 'required|min:3'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+            $response['error'] = $validator->messages();
+            return $response;
+        }
+
+        $title = $request->input('title');
+
+        $todo = new Todo();
+        $todo->title = $title;
+        $todo->save();
+
+        return $response;
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
+use Facade\FlareClient\Api;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +14,13 @@ Route::get('/ping', function (Request $request) {
         'pong' => true
     ];
 });
+
+Route::post('/todo', [ApiController::class, 'store']);
+
+Route::get('/todos', [ApiController::class, 'index']);
+
+Route::get('/todo/{id}', [ApiController::class, 'show']);
+
+Route::put('/todo/{id}', [ApiController::class, 'update']);
+
+Route::delete('/todo/{id}', [ApiController::class, 'delete']);
